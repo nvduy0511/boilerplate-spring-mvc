455e14da61c82ed1ed714d6c50218f94b608bcfe
nvduy0511 <nvduy.k19hutech@gmail.com>
2022-10-01 17:30:32 +0700

    hibernate spring-mvc

--- src/main/java/com/boilerplatespringmvc/controller/HomeController.java
+++ src/main/java/com/boilerplatespringmvc/controller/HomeController.java
@@ -1,17 +1,39 @@
 package com.boilerplatespringmvc.controller;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import javax.servlet.http.HttpServletRequest;
+import javax.transaction.Transactional;
 
-import org.springframework.stereotype.Controller;
+import com.boilerplatespringmvc.entities.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@Transactional
+@RestController
 public class HomeController {
-	@RequestMapping("/")  
-    public String display()  
-    {  
-        return "index";  
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+
+	@RequestMapping("/getuser")
+    public List<User> display()
+    {
+		Session session = sessionFactory.getCurrentSession();
+		CriteriaBuilder cb = session.getCriteriaBuilder();
+		CriteriaQuery< User > cq = cb.createQuery(User.class);
+		Root< User > root = cq.from(User.class);
+		cq.select(root);
+		Query query = session.createQuery(cq);
+    	return query.getResultList();
     }    
 	
 	@RequestMapping("/home")
---
+++ src/main/java/com/boilerplatespringmvc/entities/User.java
@@ -0,0 +1,54 @@
+package com.boilerplatespringmvc.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "pass")
+    private String pass;
+
+    @Column(name = "fullName")
+    private String fullName;
+
+    public User() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
